5ff5e97d01879456025db24b1c456a87b6f4de47
Michael Ryleev <gmar@google.com>
2015-05-18 15:43:05 -0700

    [vexpress-a15-trusty] Add vexpress-a15-trusty config
    Change-Id: I0f386f598dca0a7c7bb1ee2466a0b7d384d9d8c6
    Signed-off-by: Arve Hjønnevåg <arve@android.com>

---
+++ project/vexpress-a15-trusty.mk
@@ -0,0 +1,36 @@
+# Copyright (C) 2013 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+LOCAL_DIR := $(GET_LOCAL_DIR)
+
+TARGET := vexpress-a15
+
+MODULES += \
+	lib/trusty \
+	app/shell \
+	app/tests \
+
+USER_TASKS := sample/skel
+USER_TASKS += sample/skel2
+
+USER_TASK_LINKER_SCRIPT := $(BUILDDIR)/user_task-trusty.ld
+USER_TASK_CRTBEGIN_OBJS := $(BUILDDIR)/user/crtbegin.o
+USER_TASK_CRTEND_OBJS := $(BUILDDIR)/user/crtend.o
+
+# force enums to be 4bytes
+GLOBAL_CFLAGS += -mabi=aapcs-linux
+
+GLOBAL_DEFINES += \
+	WITH_MMU_RELOC=1
